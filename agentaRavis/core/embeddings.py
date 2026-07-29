20801644cc731c30e258bb38551ca1f86d1ef705
unknown <kave.rafiee.eng@gmail.com>
2026-07-29 19:49:19 +0330

    a

--- agentaRavis/core/embeddings.py
+++ agentaRavis/core/embeddings.py
@@ -3,7 +3,10 @@ import os
 from langchain_openai import  OpenAIEmbeddings
 
 embeddings = OpenAIEmbeddings(
-    model="text-embedding-3-large",
-    api_key=os.getenv("OPENAI_KEY"),
-    base_url=os.getenv("BASE_URL"),
+    # model="text-embedding-3-large",
+    # api_key=os.getenv("OPENAI_KEY"),
+    # base_url=os.getenv("BASE_URL"),
+    model="Embedding-3-Large",
+    api_key=os.getenv("ARVAN_API_KEY"),
+    base_url="https://arvancloudai.ir/gateway/models/Embedding-3-Large/Ua02cuX3b4aq8ZhgyR0C3OFAg7O0cneTH-zojCzHpg7mPMJN1zUMbF0s1eA0OLwrGk9alnkvqo3fGcBkLjezBMgpGGqy44D8G2E3nLODf6KCf3HzComW4TssHOGKE9LWhdjlWjKJZfrBlGePdsdmZcMO1C1Je149W_3Zwjwpy-mB54Gbk_x_xnVRmc7Us04Q1UaAmTwG8EqOVJ5TeRPZYzOBtVCldBzwDvV6uYRjb9z1lxzh6oaSKJbd9k-K8VlczjhGbwX72ys/v1"
 )
--- agentaRavis/core/llms.py
+++ agentaRavis/core/llms.py
@@ -6,12 +6,15 @@ llm = ChatOpenAI(
     # model='gpt-5-nano',
     # model='gpt-4o-mini',
     # model="gapgpt-qwen-3.5-thinking",
-    model="gpt-5-mini",
+    model="GPT-4.1-Mini",
     # model="qwen3-235b-a22b",
     # model = 'deepseek-v4-flash',
-    temperature=0.3,
-    api_key=os.getenv("OPENAI_KEY"),
-    base_url=os.getenv("BASE_URL"),
+
+    base_url="https://arvancloudai.ir/gateway/models/GPT-4.1-Mini/9OsdnLiTU_iiXBZwpOK3QzWFW__ES_uGi7PC7EgYnYjuy3VfT4wF1Q73FmlXRh35hbgUWSFYxHRQ1_bwbm6kO58L2TFGJf8JJxv---mUmUHRhMLNZrXkaUsEzftOvnK__6KmJnVF3HLhy_jR3zqUf6l-Lfp3TSMuKL4rs3S7wor8dqYBpcm-GTvr5MmrCupgVGuinj7ERK-PrM6kJBtCh1kOmMISB1zijOWLcR9QuLN__4et7J3JiDKqkXyhPA/v1",
+    api_key=os.getenv("ARVAN_API_KEY"),
+
+    # api_key=os.getenv("OPENAI_KEY"),
+    # base_url=os.getenv("BASE_URL"),
 )
 
 
@@ -20,8 +23,10 @@ weekllm = ChatOpenAI(
     # model="gpt-4.1-mini",
     # model='gpt-5-nano',
     # model="gapgpt-qwen-3.5",
-    model="gpt-4o-mini",
+    model="GPT-4.1-Mini",
     temperature=0.3,
-    api_key=os.getenv("OPENAI_KEY"),
-    base_url=os.getenv("BASE_URL"),
+    # api_key=os.getenv("OPENAI_KEY"),
+    # base_url=os.getenv("BASE_URL"),
+    base_url="https://arvancloudai.ir/gateway/models/GPT-4.1-Mini/9OsdnLiTU_iiXBZwpOK3QzWFW__ES_uGi7PC7EgYnYjuy3VfT4wF1Q73FmlXRh35hbgUWSFYxHRQ1_bwbm6kO58L2TFGJf8JJxv---mUmUHRhMLNZrXkaUsEzftOvnK__6KmJnVF3HLhy_jR3zqUf6l-Lfp3TSMuKL4rs3S7wor8dqYBpcm-GTvr5MmrCupgVGuinj7ERK-PrM6kJBtCh1kOmMISB1zijOWLcR9QuLN__4et7J3JiDKqkXyhPA/v1",
+    api_key=os.getenv("ARVAN_API_KEY"),
 )
